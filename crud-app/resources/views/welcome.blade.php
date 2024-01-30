40592c1920ee60f29d5793905d3b975de521f49b
Josué Salazaku <josuesalazaku@gmail.com>
2024-01-30 12:27:03 +0100

    delete crud file

--- crud-app/resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-
-        @vite('resources/css/app.css')
-        @extends('layouts')
-
-@section('content')
-    <!-- Your specific page content goes here -->
-    <h1 class="text-6xl">Welcome</h1>
-    <p>This is the home page content.</p>
-@endsection
